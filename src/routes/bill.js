b953c3a057c716cd269ca2472ccce590eafa8186
teeraphat0806 <110732968+teeraphat0806@users.noreply.github.com>
2024-04-02 18:21:33 +0700

    v4

--- src/routes/bill.js
+++ src/routes/bill.js
@@ -10,25 +10,28 @@ const route = Router();
 
 route.get("/", async (req, res) => {
   //ดูบิล
-  const order = await prisma.order.findMany({//ค้นหาเกมทั้งหมด
+  const order = await prisma.order.findMany({
+    //ค้นหาเกมทั้งหมด
     where: { userId: req.user.id },
   });
-  const billData = await Promise.all(//สร้างloopทั้งหมด
+  const billData = await Promise.all(
+    //สร้างloopทั้งหมด
     order.map(async (v) => {
       return prisma.bill.findFirst({
-        where: { orderId: v.id },//เก็บบิลทั้งหมดไว้ในbillData
+        where: { orderId: v.id }, //เก็บบิลทั้งหมดไว้ในbillData
       });
     })
   );
   console.log(billData);
-  return res.send(billData);//ส่งบิลทั้งหมดกลับไป
+  return res.send(billData); //ส่งบิลทั้งหมดกลับไป
 });
 
 route.get("/order/:id", async (req, res) => {
   try {
-    const schema = Joi.number().required();//กำหนดข้อมูลที่จะรับมา
+    const schema = Joi.number().required(); //กำหนดข้อมูลที่จะรับมา
     const { error, value } = schema.validate(req.params.id);
-    if (error) {//ตรวจสอบข้อผิดพลาดจากการรับข้อมูลมาถ้ามีerrorให้แสดงerror
+    if (error) {
+      //ตรวจสอบข้อผิดพลาดจากการรับข้อมูลมาถ้ามีerrorให้แสดงerror
       return res.status(400).send({ error: "Invalid id" });
     }
     // ค้นหาorderของuserจากidที่ส่งมา
@@ -38,12 +41,14 @@ route.get("/order/:id", async (req, res) => {
         Game: true, // Include the related game data
       },
     });
-    if (!order) {//ถ้าไม่พบorderให้แสดงข้อความบอก
+    if (!order) {
+      //ถ้าไม่พบorderให้แสดงข้อความบอก
       return res.status(404).send({ error: "Order not found" });
     }
     console.log(order);
-    return res.send(order);//ส่งข้อมูลorderกลับไป
-  } catch (error) {//ถ้ามี error ให้แสดง error
+    return res.send(order); //ส่งข้อมูลorderกลับไป
+  } catch (error) {
+    //ถ้ามี error ให้แสดง error
     console.error(error);
     res.status(500).send("Internal Server Error");
   }
@@ -52,20 +57,23 @@ route.get("/order/:id", async (req, res) => {
 // Route to retrieve a bill by ID
 route.get("/:id", async (req, res) => {
   try {
-    const schema = Joi.number().required();//กำหนดข้อมูลที่จะรับมา
+    const schema = Joi.number().required(); //กำหนดข้อมูลที่จะรับมา
     const { error, value } = schema.validate(req.params.id);
-    if (error) {//ตรวจสอบข้อผิดพลาดจากการรับข้อมูลมาถ้ามีerrorให้แสดงerror
+    if (error) {
+      //ตรวจสอบข้อผิดพลาดจากการรับข้อมูลมาถ้ามีerrorให้แสดงerror
       return res.status(400).send({ error: "Invalid id" });
     }
     // Assuming `bill` is the name of the model representing bills
-    const bills = await prisma.bill.findUnique({//หาbillผ่านid bill ที่ส่งมา
+    const bills = await prisma.bill.findUnique({
+      //หาbillผ่านid bill ที่ส่งมา
       where: { id: parseInt(value) },
     });
-    if (!bills) {//ถ้าไม่มีบิลให้แสดงว่าไม่พบบิล
+    if (!bills) {
+      //ถ้าไม่มีบิลให้แสดงว่าไม่พบบิล
       return res.status(404).send({ error: "Bill not found" });
     }
     console.log(bills);
-    return res.send(bills);//ส่งข้อมูลbillกลับไป
+    return res.send(bills); //ส่งข้อมูลbillกลับไป
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
@@ -74,33 +82,38 @@ route.get("/:id", async (req, res) => {
 
 route.post("/all", async (req, res) => {
   //ดูorder
-  const order = await prisma.order.findMany({//ค้นหาorderของuserผ่านtoken
+  const order = await prisma.order.findMany({
+    //ค้นหาorderของuserผ่านtoken
     where: { userId: req.user.id },
   });
-  if (order.length === 0) {//ถ้าไม่มีorderให้ส่งข้อมูลกลับมาว่าไม่พบ
+  if (order.length === 0) {
+    //ถ้าไม่มีorderให้ส่งข้อมูลกลับมาว่าไม่พบ
     return res.send("Order not found");
   }
   console.log(order);
-  return res.send(order);//ส่งข้อมูลorderกลับไป
+  return res.send(order); //ส่งข้อมูลorderกลับไป
 });
 
 route.post("/", async (req, res) => {
   //ซื่อเกมทีละเกม
-  const schema = Joi.object({//กำหนดข้อมูลที่จะรับมา
+  const schema = Joi.object({
+    //กำหนดข้อมูลที่จะรับมา
     gameId: Joi.number().required(),
   }).required();
 
-  const key = crypto.randomBytes(20).toString("hex");//สร้างker game
+  const key = crypto.randomBytes(20).toString("hex"); //สร้างker game
   const { error, value } = schema.validate(req.body);
 
-  if (error) {//ตรวจสอบข้อผิดพลาดจากการรับข้อมูลมาถ้ามีerrorให้แสดงerror
+  if (error) {
+    //ตรวจสอบข้อผิดพลาดจากการรับข้อมูลมาถ้ามีerrorให้แสดงerror
     return res.status(400).send({ error: "Invalid body" });
   }
   //ค้นหาเกมที่จะซื้อผ่านId
   const game = await prisma.game.findUnique({
     where: { id: value.gameId },
   });
-  if (!game) {//ถ้าไม่พบเกมให้แจ้งเตือนว่าไม่พบ
+  if (!game) {
+    //ถ้าไม่พบเกมให้แจ้งเตือนว่าไม่พบ
     return res.status(404).send({
       error: "Game not found",
     });
@@ -112,17 +125,20 @@ route.post("/", async (req, res) => {
       gameId: game.id,
     },
   });
-  if (check) {//ถ้าuserมีเกมที่จะซื้ออยู่ให้ส่งกลับเป็นfalse
+  if (check) {
+    //ถ้าuserมีเกมที่จะซื้ออยู่ให้ส่งกลับเป็นfalse
     return res.send({ check: false });
   }
-  await prisma.cart.deleteMany({//ลบเกมที่ซื้อออกจากตะกร้า
+  await prisma.cart.deleteMany({
+    //ลบเกมที่ซื้อออกจากตะกร้า
     where: {
       userId: req.user.id,
       gameId: game.id,
     },
   });
-  
-  const update = await prisma.game.update({//updateข้อมุลยอดขายเพิ่ม1
+
+  const update = await prisma.game.update({
+    //updateข้อมุลยอดขายเพิ่ม1
     where: {
       id: game.id,
     },
@@ -132,7 +148,8 @@ route.post("/", async (req, res) => {
   });
   console.log(value);
   try {
-    const bill = await prisma.bill.create({//สร้างบิลและorder
+    const bill = await prisma.bill.create({
+      //สร้างบิลและorder
       data: {
         price: game.price,
         Order: {
@@ -146,8 +163,9 @@ route.post("/", async (req, res) => {
     });
 
     console.log(bill);
-    return res.send(bill.id);//ส่งbillId กลับไป
-  } catch (error) {//ถ้ามีerrorให้ส่งerrorกลับไป
+    return res.send(bill); //ส่งbill กลับไป
+  } catch (error) {
+    //ถ้ามีerrorให้ส่งerrorกลับไป
     console.log(error);
     if (error instanceof Prisma.PrismaClientInitializationError) {
       if (error.mata.target) {
@@ -163,18 +181,22 @@ route.post("/", async (req, res) => {
 route.post("/cart", async (req, res) => {
   //ซื้อเกมในตะกร้า
   try {
-    const cart = await prisma.cart.findMany({//ค้นหาเกมในตะกร้าของuser
+    const cart = await prisma.cart.findMany({
+      //ค้นหาเกมในตะกร้าของuser
       where: { userId: req.user.id },
     });
 
-    const billData = await Promise.all(//สร้างbillหลายๆbill
+    const billData = await Promise.all(
+      //สร้างbillหลายๆbill
       cart.map(async (v) => {
-        const key = crypto.randomBytes(20).toString("hex");//สร้างker game
-        const game = await prisma.game.findUnique({//ค้นหาเกมที่จะซื้อ
+        const key = crypto.randomBytes(20).toString("hex"); //สร้างker game
+        const game = await prisma.game.findUnique({
+          //ค้นหาเกมที่จะซื้อ
           where: { id: v.gameId },
         });
-    
-        await prisma.cart.delete({//ลบเกมที่ซื้อออกจากตะกร้า
+
+        await prisma.cart.delete({
+          //ลบเกมที่ซื้อออกจากตะกร้า
           where: { id: v.id },
         });
         //ตรวจสอบว่าuserมีเกมที่จะซื้ออยู่แล้วหรือไม่
@@ -197,7 +219,7 @@ route.post("/cart", async (req, res) => {
           },
         });
         //สร้างบิลและorder
-        const createdBill = await prisma.bill.create({
+        return await prisma.bill.create({
           data: {
             price: game.price,
             Order: {
@@ -209,13 +231,13 @@ route.post("/cart", async (req, res) => {
             },
           },
         });
-        return { id: createdBill.id }; // ส่งค่า id ของบิลที่สร้างขึ้น
       })
     );
 
     console.log(billData);
-    return res.send(billData);//ส่งbill Id ทั้งหมดกลับไป
-  } catch (e) {//ถ้ามีerrorให้ส่งerrorกลับไป
+    return res.send(billData); //ส่งbill Id ทั้งหมดกลับไป
+  } catch (e) {
+    //ถ้ามีerrorให้ส่งerrorกลับไป
     console.log(e);
     if (e instanceof Prisma.PrismaClientKnownRequestError) {
       if (e.meta.target) {
